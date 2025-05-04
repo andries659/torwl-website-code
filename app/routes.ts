48966b1a1cdbf01fe4008ef103145d8cfb1fab78
AndriesWorks <andries46565@gmail.com>
2025-05-04 10:45:32 +0200

    added new routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,8 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-  index("routes/home/home.tsx"),  // Adds your home page at the root '/'
-  route("/roles", "routes/roles/roles.tsx"),  // Adds your about page at '/about'
+  index("routes/home/home.tsx"),
+  route("/roles", "routes/roles/roles.tsx"),
+  route("/events-bot/privacy-policy", "routes/events-bot/privacy-policy.tsx"),
+  route("/events-bot/terms-of-service", "routes/events-bot/terms-of-service.tsx"),
 ] satisfies RouteConfig;
