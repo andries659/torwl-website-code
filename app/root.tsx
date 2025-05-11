110f4472531b580740f1c02dda534010d7d3e281
AndriesWorks <168019743+andries659@users.noreply.github.com>
2025-05-11 23:25:08 +0200

    Mobile test first

--- app/root.tsx
+++ app/root.tsx
@@ -119,7 +119,16 @@ export default function App() {
             )}
           </div>
 
-          <a href="https://github.com/andries659/test-website" target="_blank" rel="noopener noreferrer" className="underline" onClick={() => setIsOpen(false)}>Contribute!</a>
+          <a
+  href="https://github.com/andries659/test-website"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="underline flex items-center gap-1"
+  onClick={() => setIsOpen(false)}
+>
+  Contribute!
+  <span className="material-symbols-outlined text-sm align-middle">open_in_new</span>
+</a>
         </nav>
       </aside>
 
