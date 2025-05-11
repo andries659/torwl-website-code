5d8b425511795b6a73eea235671f601fdf040e00
AndriesWorks <168019743+andries659@users.noreply.github.com>
2025-05-11 18:03:54 +0200

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -5,4 +5,6 @@ export default [
   route("/roles", "routes/roles/roles.tsx"),
   route("/events-bot/privacy-policy", "routes/events-bot/privacy-policy.tsx"),
   route("/events-bot/terms-of-service", "routes/events-bot/terms-of-service.tsx"),
+  route("/torw-bot/privacy-policy", "routes/torw-bot/privacy-policy.tsx"),
+  route("/torw-bot/privacy-policy", "routes/torw-bot/terms-of-service.tsx"),
 ] satisfies RouteConfig;
