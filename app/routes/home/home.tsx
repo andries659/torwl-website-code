dda9891f397a76d63c0a3969b66e715748edf586
AndriesWorks <andries46565@gmail.com>
2025-05-16 22:23:51 +0200

    Update home.tsx

--- app/routes/home/home.tsx
+++ app/routes/home/home.tsx
@@ -10,7 +10,7 @@ export function meta({ }: Route.MetaArgs) {
 export default function Home() {
   return (
     <div className="text-center">
-      <img src={Logo} alt="TOR-W Logo" className="mx-auto my-6 w-32 h-auto" />
+      <img src={Logo} alt="TOR-W Logo" className="mx-auto my-6 w-128 h-auto" />
       <h1 className="text-4xl text-yellow-500 font-bold">🚀 TOR-W Launchpad</h1>
       <header className="flex flex-col items-center gap-9"><br></br>
         <a href="https://discord.com/invite/HczqtuBfcu" target="_blank"><img src="https://dcbadge.limes.pink/api/server/HczqtuBfcu" /></a>
